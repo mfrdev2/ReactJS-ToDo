361e6ced244f52f08d05c2f594648a14c446e2c2
mfrrabbi <mdfazlerabbi.diu@gmail.com>
2021-10-07 17:37:13 +0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1 class="heading">Todo App</h1>
+    <div className = "card">
+      <h3 className= "cardTitle">{"Todo-Title"}</h3>
+      <p className = "cardDesc">Kasd accusam ipsum accusam dolor invidunt et dolor ea voluptua sanctus. Diam eirmod sit nonumy magna voluptua invidunt. Sanctus duo.</p>
+      <p className = "cardTime">{timePrint()}</p>
+    </div>
+  </div>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+function timePrint() {
+  const today = new Date();
+  const day = today.toLocaleDateString('en', { weekday: 'long' });
+  const dayMonth = `${day}, ${today.getDate()} ${today.toLocaleDateString('en', { month: 'long' })}\n\n`;
+  const year = today.getFullYear();
+  const hour = today.getHours();
+  const wish = `Good ${(hour < 12 && 'Morning') || (hour < 17 && 'Afternoon') || 'Evening'} `;
+
+  const time = today.toLocaleTimeString('en', { hour: 'numeric', hour12: true, minute: 'numeric' });
+
+  const timePrint = dayMonth + " " + year + ": " + time + ", " + wish;
+  return timePrint;
+}
+
